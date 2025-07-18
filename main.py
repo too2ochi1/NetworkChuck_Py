8caf11b26c245c0ed1d0dd38534e42d84a91b967
too2ochi1 <herbertj289@gmail.com>
2025-07-18 20:59:08 +0100

    Playing around with some elementary python concepts. May or may not be subject to tweaks/improvements

---
+++ main.py
@@ -0,0 +1,33 @@
+
+
+print("Hello World!!!!!!!!!!!!!!!!!!!!!!!!")
+print('I am Batman')
+print("No, I am Bruce Wayne")
+print("I am Kuro-chan")
+#We are practicing python... IT'S AWESOME!!!
+
+print ("""
+Hello World!!!!!!!!!!!!!!!!!!!!!!!!
+I am Batman
+No, I am Bruce Wayne
+No, I am Kuro-chan
+""")
+#Multi-line strings can be used when trying to put multiple sentences to stand alone in python
+
+
+#Concatenations are basically when you try to add multiple strings together usin the "+" symbol
+
+print("Hi" + "I'm" + "Jason")
+#When you run this line directly aove this comment the way it is, the result is : HiI'mJason, Like literally. So you need to add spaces within the quotes of the strings like:
+
+print("I"+ " like" + " bread")
+
+#OR
+print("Hi" + "\nI'm" + " \nJason")
+#The character "\n" is known as a new line indicator
+
+#python also has word problems. For example:
+print("I will learn Python\n" * 1000)
+
+
+
