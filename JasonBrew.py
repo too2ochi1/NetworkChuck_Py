71d6d2e6cf2281c5b2d3c1e897fda6e3e1ed2d0a
too2ochi1 <herbertj289@gmail.com>
2025-07-18 20:59:25 +0100

    The beginning of my virtual coffee shop, adventures with my python-based RoboBarista, or RB for short. Will make improvements down the line

---
+++ JasonBrew.py
@@ -0,0 +1,22 @@
+print("Hello and welcome to JasonBrew!!!!!")
+
+name = input("What is your name?\n")
+
+print("Hello " + name + ", thanks for coming in today.")
+print("""Here's our menu:
+    1. Black Coffee
+    2. Latte
+    3. Cafe Mocha""")
+
+drink = input("What would you like to drink?\n")
+
+price = 500
+
+print("Alright then, " + drink + " costs #" + str(price) + " per cup.")
+
+number = input("How many cups would you be needing?\n")
+
+total_cost = int(number) * price
+
+print("That'd be a total of #" + str(total_cost) )
+print("Here you go, enjoy your " + number + " cups of "+ drink + "!")
