b4d5ca0aaf2f22804e6557c8a1b2ebe94dfdca02
too2ochi1 <herbertj289@gmail.com>
2025-07-20 13:19:21 +0100

    Still on activities of RoboBarista (RB for short) at JasonBrew. We learned to kick out an unwelcome entity from our coffee shop using if-else statements and an end function. More changes to come

--- JasonBrew.py
+++ JasonBrew.py
@@ -2,21 +2,25 @@ print("Hello and welcome to JasonBrew!!!!!")
 
 name = input("What is your name?\n")
 
-print("Hello " + name + ", thanks for coming in today.")
-print("""Here's our menu:
-    1. Black Coffee
-    2. Latte
-    3. Cafe Mocha""")
+if name == "Ben":
+    print("You're not welcome here. Go away")
+    exit()
+else:
+    print("Hello " + name + ", thanks for coming in today.")
+    print("""Here's our menu:
+        1. Black Coffee
+        2. Latte
+        3. Cafe Mocha""")
 
-drink = input("What would you like to drink?\n")
+    drink = input("What would you like to drink?\n")
 
-price = 500
+    price = 500
 
-print("Alright then, " + drink + " costs #" + str(price) + " per cup.")
+    print("Alright then, " + drink + " costs #" + str(price) + " per cup.")
 
-number = input("How many cups would you be needing?\n")
+    number = input("How many cups would you be needing?\n")
 
-total_cost = int(number) * price
+    total_cost = int(number) * price
 
-print("That'd be a total of #" + str(total_cost) )
-print("Here you go, enjoy your " + number + " cups of "+ drink + "!")
+    print("That'd be a total of #" + str(total_cost) )
+    print("Here you go, enjoy your " + number + " cups of "+ drink + "!")
